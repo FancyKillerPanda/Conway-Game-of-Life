1d0da123e14840d2fc07ce94659224d344f06c91
Sahil Iyer <iyersahil12@gmail.com>
2019-01-07 22:56:03 +1100

    Set-up SFML and opened a window.

--- Conway's Game of Life/include/Game.h
+++ Conway's Game of Life/include/Game.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
+
 class Game
 {
+private:
+	sf::RenderWindow m_Window;
+	sf::Event m_Event;
+
 public:
 	Game();
 
 	void run();
+
+	void handleEvents();
+	void update();
+	void draw();
 };
--- Conway's Game of Life/src/Game.cpp
+++ Conway's Game of Life/src/Game.cpp
@@ -1,11 +1,45 @@
 #include "Game.h"
 
+#include "Core.h"
+
 
 Game::Game()
+	: m_Window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Conway's Game of Life!")
 {
 }
 
 
 void Game::run()
 {
+	while (m_Window.isOpen())
+	{
+		handleEvents();
+		update();
+		draw();
+	}
+}
+
+
+void Game::handleEvents()
+{
+	while (m_Window.pollEvent(m_Event))
+	{
+		switch (m_Event.type)
+		{
+		case sf::Event::Closed:
+			m_Window.close();
+			break;
+		}
+	}
+}
+
+void Game::update()
+{
+}
+
+void Game::draw()
+{
+	m_Window.clear(sf::Color::Red);
+
+	m_Window.display();
 }
